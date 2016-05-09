0a72c9b577671207c3cc80e8f40f0ee6429d999b
Jian Yuan Lee <jianyuan@gmail.com>
2016-05-09 10:59:21 +0100

    remove network after testing

--- build.sh
+++ build.sh
@@ -144,6 +144,7 @@ else
 		RET=$(docker wait ${PROJECT_NAME}_sut_1)
 		docker-compose -f ${TEST_FILENAME} -p $PROJECT_NAME kill
 		docker-compose -f ${TEST_FILENAME} -p $PROJECT_NAME rm --force -v
+		docker network ls --filter name=${PROJECT_NAME} --quiet | xargs docker network rm
 		if [ "$RET" != "0" ]; then
 			print_msg "   Tests in $TEST_FILENAME FAILED: $RET"
 			exit 1
